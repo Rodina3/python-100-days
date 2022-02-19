09cfcbb14d629a1ad7b6b2fabf06812a0b11b127
Rong <buaazhourong@163.com>
2022-02-19 13:45:12 +0800

    feat: draw shapes

---
+++ day-18/draw_shapes.py
@@ -0,0 +1,20 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+# Draw Shapes
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightDeaGrean", "wheat", "SlateGray",
+          "SeaGreen"]
+
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_in in range(3, 10):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_in)
