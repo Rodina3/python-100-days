2ba25bb9337016255180968eed23104205d46109
Rong <buaazhourong@163.com>
2022-02-27 14:43:51 +0800

    [flash_card_app] feat: flip cards

--- day-31/main.py
+++ day-31/main.py
@@ -7,20 +7,35 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pandas.read_csv("data/frequency_words.csv")
 to_learn = data.to_dict(orient="records")
 
+current_word = {}
+
 
 def next_card():
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
     current_word = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=current_word["English"])
+    canvas.itemconfig(card_title, text="English", fill="black")
+    canvas.itemconfig(card_word, text=current_word["English"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="Chinese", fill="white")
+    canvas.itemconfig(card_word, text=current_word["Chinese"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("Remember Words")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
