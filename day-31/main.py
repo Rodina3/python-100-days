f353d9a330badab04d5b7212877b566122930cc5
Rong <buaazhourong@163.com>
2022-02-27 14:21:58 +0800

    [flash_card_app] feat: create new flash card

--- day-31/main.py
+++ day-31/main.py
@@ -1,25 +1,39 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/frequency_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_word = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_word["English"])
+
 
 window = Tk()
 window.title("Remember Words")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="./images/card_front.png")
+card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-cross_img = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=cross_img, highlightthickness=0)
+cross_img = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
-check_img = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_img, highlightthickness=0)
+check_img = PhotoImage(file="images/right.png")
+known_button = Button(image=check_img, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
